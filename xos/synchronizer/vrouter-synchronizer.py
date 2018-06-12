0310ca184971a7325aaf32c16b3e40628bf290f7
Matteo Scandolo <teo@opennetworking.org>
2018-06-12 12:15:19 -0700

    [CORD-3022] Splitting config
    Change-Id: I841d7173729a1c3eb98c098e0b3b6a90de18dcba

--- xos/synchronizer/vrouter-synchronizer.py
+++ xos/synchronizer/vrouter-synchronizer.py
@@ -21,9 +21,13 @@ import os
 import sys
 from xosconfig import Config
 
-config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/vrouter_config.yaml')
+base_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/config.yaml')
+mounted_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/mounted_config.yaml')
 
-Config.init(config_file, 'synchronizer-config-schema.yaml')
+if os.path.isfile(mounted_config_file):
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml', mounted_config_file)
+else:
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml')
 
 synchronizer_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../synchronizers/new_base")
 sys.path.append(synchronizer_path)
